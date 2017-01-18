02dc50a5b99f9df724927d87116d0d11ca9ae9f8
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-18 13:06:35 +0300

    Passed 3 failed 2 tests

---
+++ string_length.py
@@ -0,0 +1,8 @@
+def string_length(d):
+	if isinstance(d, (list)) & len(d) > 1:
+		for x in d:
+  			return [len(x[0:-1])]
+  	elif len(d) >= 1:
+  		return [len(d)]
+  	else:
+  		return 'Invalid input'
